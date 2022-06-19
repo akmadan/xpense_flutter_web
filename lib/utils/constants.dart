3a3c91f292e5ee330b55f0427fc78bae78202f58
Akshit Madan <akmadan1717@gmail.com>
2022-06-19 13:55:41 +0530

    images

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,10 @@ double? w;
 //Images
 const String logo = 'assets/images/logo.png';
 const String illustration1 = 'assets/images/illustration1.png';
+const String illustration2 = 'assets/images/illustration2.png';
+const String illustration3 = 'assets/images/illustration3.png';
+const String google = 'assets/images/google.png';
+const String fb = 'assets/images/fb.png';
+const String cocacola = 'assets/images/cocacola.png';
+const String samsung = 'assets/images/samsung.png';
+const String dashboard = 'assets/images/dashboard.png';
